7c1f684da4902fc50847ca63912da81bb6d2f189
sickandpanik <shadenis2015@gmail.com>
2025-05-03 07:27:07 +0200

    Complete task

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# verlag-fuer-gedichte
+# Verlag für Gedichte—“Poem Publisher”
+
+This is a smart contract which publishes poems as ERC721 smart contracts
+and returns a non-fungible token to the owner. IPFS is used for storage
+of the poems.
+
+## Deploying to Sepolia
+
+
+
+## References
+
+[How to Create and Deploy an ERC-721 (NFT) | QuickNode Guides](https://www.quicknode.com/guides/ethereum-development/nfts/how-to-create-and-deploy-an-erc-721-nft)
+
+[ERC-721 - OpenZeppelin Docs](https://docs.openzeppelin.com/contracts/5.x/erc721)
---
+++ contracts/PoemNFT.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract Poem is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("Gedicht", "GDCHT")
+        Ownable(initialOwner)
+    {}
+
+    function publishAPoem(address to, string memory uri)
+        public
+        onlyOwner
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
